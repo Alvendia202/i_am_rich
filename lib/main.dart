0c80a7182b5ed89db1a36dcd8b6b91cb8f14ff6b
Alvendia202 <Marymelodyalvendia@gmail.com>
2022-12-14 01:39:45 +0800

    changes

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I am RICH'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image:
+            AssetImage('images/diamond.gif'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
